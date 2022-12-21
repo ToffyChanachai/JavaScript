057990194d6be6fce630a18c84e9647b70649cb8
ToffyChanachai <toffy.chanachai123@gmail.com>
2022-12-21 09:34:15 +0700

    create index.html

---
+++ Lecture-02/datatypes.js
@@ -0,0 +1,17 @@
+const people = ["Aaron", "Mel", "John"];
+const one = 1;
+const str = "Hello World";
+const b = true;
+const employeee = {
+    firstName: "Anirach",
+    lastName: "Mingkhawn",
+};
+
+function sayHello(person) {
+    crossOriginIsolated.log("Hello" + person.firstName);
+}
+
+console.log(typeof people);
+console.log(typeof sayHello)
+console.log(employeee instanceof Array);
+sayHello(employeee)
