e880b47e4eb167dfdab2a6483bf00a4d71392572
ToffyChanachai <toffy.chanachai123@gmail.com>
2022-12-28 09:57:03 +0700

    update

--- Lecture-02/Week-2/parsefloat.js
+++ Lecture-02/Week-2/parsefloat.js
@@ -5,7 +5,7 @@ function circumference(r) {
     return parseFloat(r) * 2.0 * Math.PI;
 }
 
-console.log(circumference("4.234abcdefgh"));
+console.log(circumference("4.567abcdefgh"));
 // expected output: 28.695307297889173
 
 console.log(circumference("abcdefgh"));
