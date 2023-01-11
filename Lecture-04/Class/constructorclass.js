53b5409fdfd0893b548d38891b47724dd0e975b6
ToffyChanachai <toffy.chanachai123@gmail.com>
2023-01-11 11:02:52 +0700

    update

---
+++ Lecture-04/Class/constructorclass.js
@@ -0,0 +1,9 @@
+class Song {
+    constructor(titlr, artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+}
+
+const mySong = new Song = new Song("Bohemian Rhapsody", "Queen");
+console.log(mySong.title);
